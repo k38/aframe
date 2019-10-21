08ee8ab0550d611c173193df16f40302daff18ec
k38 <matumoto.stc@gmail.com>
2019-10-22 01:40:34 +0900

    Add: Entity-Component-System (ecs)

---
+++ ecs/index.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', DOMContentLoaded);
+
+function DOMContentLoaded(){
+    // AFRAME.registerComponent('foo', {
+    //     schema: {
+    //         bar: {type: 'number'},
+    //         baz: {type: 'string'}
+    //     },
+    
+    //     init: function () {
+    //         // Do something when component first attached.
+    //         console.log("foo init");
+    //     },
+    
+    //     update: function () {
+    //         // Do something when component's data is updated.
+    //     },
+    
+    //     remove: function () {
+    //         // Do something the component or its entity is detached.
+    //     },
+    
+    //     tick: function (time, timeDelta) {
+    //         // Do something on every scene tick or frame.
+    //     }
+    // });
+    
+    // AFRAME.registerPrimitive('a-ocean', {
+    //     defaultComponents: {
+    //         ocean: {},
+    //         rotation: {x: -90, y: 0, z: 0}
+    //     },
+    //     mappings: {
+    //         width: 'ocean.width',
+    //         depth: 'ocean.depth',
+    //         density: 'ocean.density',
+    //         color: 'ocean.color',
+    //         opacity: 'ocean.opacity'
+    //     }
+    // });
+}
