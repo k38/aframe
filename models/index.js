5fb3406d97623bef74e944633d467d2f6dd9750e
k38 <matumoto.stc@gmail.com>
2019-11-04 15:30:30 +0900

    Add: modles

---
+++ models/index.js
@@ -0,0 +1,20 @@
+//@ts-check
+document.addEventListener('DOMContentLoaded', DOMContentLoaded);
+
+function DOMContentLoaded() {
+    AFRAME.registerComponent('modify-materials', {
+        init: function () {
+            // Wait for model to load.
+            this.el.addEventListener('model-loaded', () => {
+                // Grab the mesh / scene.
+                const obj = this.el.getObject3D('mesh');
+                // Go over the submeshes and modify materials we want.
+                obj.traverse(node => {
+                    if (node.name.indexOf('ship') !== -1) {
+                        node.material.color.set('red');
+                    }
+                });
+            });
+        }
+    });
+}
