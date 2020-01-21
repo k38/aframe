5f56b1bd70ba5e575116e711324cfec0740957b0
k38 <matumoto.stc@gmail.com>
2020-01-22 01:07:39 +0900

    Add: particle timing

--- mushroom_spores/index.js
+++ mushroom_spores/index.js
@@ -3,4 +3,13 @@ document.addEventListener('DOMContentLoaded', DOMContentLoaded);
 
 function DOMContentLoaded(){
     document.querySelector("a-scene").renderer.gammaOutput=true;
+    const assetsEl = document.querySelector("a-assets");
+    assetsEl.addEventListener("loaded", assetsLoaded);
+}
+
+function assetsLoaded(e) {
+    const particles = document.querySelectorAll("[particle-system]");
+    particles.forEach((p)=>{
+        p.setAttribute("particle-system", "enabled", true);
+    });
 }
