aae47afba27eb9c4186cc7dd16b5b302fa01ce21
k38 <matumoto.stc@gmail.com>
2020-01-31 01:37:51 +0900

    Add: particle-system

---
+++ particle-system/index.js
@@ -0,0 +1,13 @@
+//@ts-check
+document.addEventListener('DOMContentLoaded', DOMContentLoaded);
+
+function DOMContentLoaded(){
+    // const enabled = document.querySelector("#enabled");
+    // let sw = true;
+    // setInterval(()=>{
+    //     enabled.setAttribute("particle-system", "enabled", sw);
+    //     sw = sw ? false: true;
+    // }, 3000);
+
+    
+}
